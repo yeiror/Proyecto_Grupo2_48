500781065d44ad1a83f7bb7dd0656fb837a47f9c
Alejandro Jimenez <alejandroedu71@gmail.com>
2021-09-17 15:32:53 -0500

    Sprint-2 Prototipo_1

--- proyecto_septimo_arte/src/App.vue
+++ proyecto_septimo_arte/src/App.vue
@@ -1,3 +1,4 @@
+<!-- Plantilla original
 <template>
   <v-app>
     <v-app-bar
@@ -53,3 +54,142 @@ export default {
   }),
 };
 </script>
+-->
+
+<template>
+  <v-app>
+    <v-app-bar app color="black" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="@/assets/Septimo arte/02.logo.png"
+          transition="scale-transition"
+          width="40"
+        />
+<!--
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+        -->
+        <v-toolbar-title>Septimo Arte</v-toolbar-title>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        class="my-2 ma-2"
+        v-for="link in links"
+        :key="link"
+        color="amber darken-1"
+        rounded
+        dark
+      >
+        {{ link }}
+      </v-btn>
+
+      <div class="text-center">
+    <v-menu
+      v-model="menu"
+      :close-on-content-click="false"
+      :nudge-width="200"
+      offset-x
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+        rounded
+          color="amber darken-1"
+          dark
+          v-bind="attrs"
+          v-on="on"
+        >
+                    <v-icon>mdi-account-circle</v-icon>
+
+        </v-btn>
+      </template>
+
+      <v-card>
+        <v-list>
+          <v-list-item>
+            <v-list-item-avatar>
+              <img
+                src="https://cdn.vuetifyjs.com/images/john.jpg"
+                alt="#"
+              >
+            </v-list-item-avatar>
+
+            <v-list-item-content>
+              <v-list-item-title>Alejandro Jimenez</v-list-item-title>
+              <v-list-item-subtitle>Founder of Vuetify</v-list-item-subtitle>
+            </v-list-item-content>
+
+            
+          </v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list>
+          <v-list-item>
+            <v-list-item-action>
+              <v-switch
+                v-model="message"
+                color="purple"
+              ></v-switch>
+            </v-list-item-action>
+            <v-list-item-title>Enable messages</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-action>
+              <v-switch
+                v-model="hints"
+                color="purple"
+              ></v-switch>
+            </v-list-item-action>
+            <v-list-item-title>Enable hints</v-list-item-title>
+          </v-list-item>
+        </v-list>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+
+          <v-btn
+            text
+            @click="menu = false"
+          >
+            Cancel
+          </v-btn>
+          <v-btn
+            color="primary"
+            text
+            @click="menu = false"
+          >
+            Save
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-menu>
+  </div>
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    links: ["INICIO", "CARTELERA", "PRÓXIMOS ESTRENOS", "CATERING"],
+  }),
+};
+</script>
+
