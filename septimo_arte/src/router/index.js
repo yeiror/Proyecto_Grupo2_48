d5da0aaeea9dca3407b9b36ea03baa3505aaa662
Alejandro Jimenez <alejandroedu71@gmail.com>
2021-09-24 10:53:45 -0500

    Modificaciones en el router y vistas(Listo para trabajar en las vistas)

--- septimo_arte/src/router/index.js
+++ septimo_arte/src/router/index.js
@@ -1,23 +1,35 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Inicio from '../views/Inicio.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Inicio',
+    component: Inicio
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/Cartelera',
+    name: 'Cartelera',
+    component: () => import( '../views/Cartelera.vue')
+  },
+  {
+    path: '/Proximamente',
+    name: 'Proximamente',
+    component: () => import( '../views/Proximamente.vue')
+  },
+  {
+    path: '/Catering',
+    name: 'Catering',
+    component: () => import( '../views/Catering.vue')
+  },
+  {
+    path: '/Inicio_sesion',
+    name: 'Inicio_sesion',
+    component: () => import( '../views/Inicio_sesion.vue')
+  },
 ]
 
 const router = new VueRouter({
---
+++ septimo_arte/src/views/Proximamente.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="proximamnete">
+    <h1>This is an proximamente page</h1>
+  </div>
+</template>
